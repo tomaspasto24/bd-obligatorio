b655fb1d694ca92344aa5476e69ececddec098d8
Tomas <tomaspasto24@gmail.com>
2022-11-12 16:42:38 -0300

    .

--- app2/app2/src/main/java/com/mycompany/dbconnection/DBConnection.java
+++ app2/app2/src/main/java/com/mycompany/dbconnection/DBConnection.java
@@ -4,10 +4,34 @@
  */
 package com.mycompany.dbconnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author TomasUcu
  */
 public class DBConnection {
+    Connection conectar = null;
+    
+    String usuario = "usersql";
+    String contrasena = "root";
+    String bd = "ObligatorioDB";
+    String ip = "localhost";
+    String puerto = "1434";
+    
+    String cadena = "jdbc:sqlserver://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion() {
+        try {
+            String cadena = "jdbc:sqlserver://localhost"+puerto+";"+"databaseName="+bd;
+            conectar = DriverManager.getConnection(cadena, usuario, contrasena);
+            JOptionPane.showMessageDialog(null, "Se conectó bien a la base de datos");
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos, error: " + e.toString());
+        }
+        return conectar;
+    }
     
 }
